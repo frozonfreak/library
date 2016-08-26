d0f1d147006dc1d63978c1795cd8e810c1ca4c48
Sastha <shazcool@gmail.com>
2016-08-26 22:58:07 +0800

    update user and role models update user_role relation

---
+++ app/Models/User.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Validator;
+use Hash;
+
+class User extends Authenticatable
+{   
+    use SoftDeletes;
+    private $rules = [
+                        'first_name' => 'required|max:255',
+                        'last_name' => 'required|max:255',
+                        'email' => 'required|unique:users,email|max:255',
+                        'password' => 'required|min:8|max:255',
+                        'age'=>'required',
+                    ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name', 'email', 'password', 'age'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // For JSON responses
+    protected $visible = array(
+        'first_name', 
+        'last_name',
+        'email',
+        'age',
+    );
+
+    public function validate($data)
+    {
+        // make a new validator object
+        return Validator::make($data, $this->rules);
+
+    }
+
+    public function roles()
+    {
+      return $this->belongsToMany('App\Role', 'user_roles')->withTimestamps();
+    }
+
+    public function isPasswordValid($password)
+    {
+        if (Hash::check($password, $this->password)) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function hasRole($role)
+    {
+        if($this->roles()->where('name',$role)->count() > 0)
+            return true;
+        else
+            return false;    
+    }
+}
