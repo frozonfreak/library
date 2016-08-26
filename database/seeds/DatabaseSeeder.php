219bc654d790602514686acb869a551cd80bdc76
Sastha <shazcool@gmail.com>
2016-08-26 23:16:27 +0800

    add fake data to books table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use Faker\Factory as Faker;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,6 +16,21 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+
+        $faker = Faker::create();
+        foreach (range(1,100) as $index) {
+            DB::table('books')->insert([
+                'title' => $faker->name,
+                'author' => $faker->name($gender = null|'male'|'female') ,
+                'isbn' => $faker->isbn13,
+                'quantities' => $faker->numberBetween($min = 1, $max = 20),
+                'location' => $faker->swiftBicNumber,
+                'image_url' => $faker->imageUrl($width = 640, $height = 480),
+                'created_at' => $faker->dateTimeThisMonth($max = 'now'),
+                'updated_at' => $faker->dateTimeThisMonth($max = 'now'),
+                'deleted_at' => NULL,
+            ]);
+        }
 
         Model::reguard();
     }
