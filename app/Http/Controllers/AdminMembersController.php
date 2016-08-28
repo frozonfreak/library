2bac505661fb784e2c75e110970d191d28426afa
Sastha <shazcool@gmail.com>
2016-08-28 13:49:48 +0800

    update admin member controller

--- app/Http/Controllers/AdminMembersController.php
+++ app/Http/Controllers/AdminMembersController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Purifier;
+use Auth;
+use Input;
+use File;
+use Hash;
+use App\User;
+use App\Role;
+
 class AdminMembersController extends Controller
 {
     /**
@@ -17,6 +25,11 @@ class AdminMembersController extends Controller
     public function index()
     {
         //
+        $user = Auth::user();
+
+        $members = User::paginate(15);
+
+        return view('admin.users.index', compact('user', 'members'));
     }
 
     /**
@@ -27,6 +40,9 @@ class AdminMembersController extends Controller
     public function create()
     {
         //
+        $user = Auth::user();
+        
+        return view('admin.users.create', compact('user'));
     }
 
     /**
@@ -37,7 +53,34 @@ class AdminMembersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->only('first_name', 'last_name', 'age', 'email', 'password');
+        
+        $user = new User();
+        
+        $validator = $user->validate($data);
+
+        if ($validator->fails()) {
+            return back()->with('danger', $validator->errors());
+        }
+
+        $user = User::Create(array(
+                        'first_name' => $data['first_name'],
+                        'last_name' => $data['last_name'],
+                        'age' => $data['age'],
+                        'email' => $data['email'],
+                        'password' => Hash::make($data['password'])
+                    ));
+
+        $role_user = 'student';
+        if($data['age'] < 17)
+            $role_user = 'junior_student';
+        //Get role name
+        $role = Role::where('name', $role_user)->first();
+        //If exists attach role to user
+        if(@$role)
+            $user->roles()->attach($role->id);
+
+        return redirect()->route('admin.users')->with('success', 'User Created');
     }
 
     /**
@@ -49,6 +92,11 @@ class AdminMembersController extends Controller
     public function show($id)
     {
         //
+        $user = Auth::user();
+
+        $profile = User::find($id);
+
+        return view('admin.users.create', compact('user', 'profile'));
     }
 
     /**
@@ -59,7 +107,24 @@ class AdminMembersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = Auth::user();
+
+        $profile = User::find($id);
+        
+        $data = Input::all();
+        
+        $data = array_map(function($input){
+              return Purifier::clean($input);
+            },$data);
+
+        $profile->first_name = $data['first_name'];
+        $profile->last_name = $data['last_name'];
+        $profile->email = $data['email'];
+        $profile->age = $data['age'];
+        $profile->password = Hash::make($data['password']);
+        $profile->save();
+
+        return redirect()->route('books')->with('success', 'User Updated');
     }
 
     /**
@@ -82,6 +147,10 @@ class AdminMembersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = Auth::user();   
+
+        User::where('id', $id)->delete();
+
+        return redirect()->back()->with('success', 'User Deleted');
     }
 }
