35d7938848783d075e278bb728a8c6403fd2369e
Sastha <shazcool@gmail.com>
2016-08-28 13:07:58 +0800

    update route and include notification view

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -43,13 +43,14 @@
     <!--
     <link rel="canonical" href="http://www.example.com/">
     -->
-
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.2.0/material.cyan-light_blue.min.css">
     <link rel="stylesheet" href="{{ elixir('assets/css/dashboard.css') }}">
   </head>
   <body>
+    @include('components.notification')
     <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
       <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
         <div class="mdl-layout__header-row">
@@ -91,6 +92,10 @@
           </div>
         </header>
         <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
+          @if($user->hasRole('admin'))
+            <a class="mdl-navigation__link" href="{{URL::route('admin.books')}}"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Books</a>
+            <a class="mdl-navigation__link" href="{{URL::route('admin.members')}}"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Members</a>
+          @endif
           <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
           <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Inbox</a>
           <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">delete</i>Trash</a>
