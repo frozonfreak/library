40446000bb941e5c551cc9b25bba1451aeea1c47
Sastha <shazcool@gmail.com>
2016-08-28 16:13:53 +0800

    create user books controller

---
+++ app/Http/Controllers/UserBooksController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Auth;
+use App\User;
+use App\Book;
+use Redirect;
+
+
+class UserBooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $user = Auth::user();
+
+        return view('users.books.index', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $user = Auth::user();
+
+        $book = Book::find($id);
+
+        if(!$book){
+            return Redirect::route('books')->with('danger', 'Not Found');
+        }
+
+        return view('users.books.show', compact('user', 'book'));   
+    }
+
+    public function submit($id)
+    {
+        $user = Auth::user();
+
+        $book = Book::find($id);
+
+        if(!$book){
+            return Redirect::route('books')->with('danger', 'Not Found');
+        }
+
+        $user->books()->detach($id);
+
+        return Redirect::back()->with('success', 'Book returned back');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $user = Auth::user();
+
+        $borrowed_books = $user->books()->lists('books.id')->toArray();
+        
+        //Display error if already borrowed
+        if($borrowed_books && in_array($id, $borrowed_books)){
+            return Redirect::back()->with('danger', 'Already borrowed');
+        }
+
+        $user->books()->attach($id);
+        
+        return Redirect::back()->with('success', 'Book borrowed');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
